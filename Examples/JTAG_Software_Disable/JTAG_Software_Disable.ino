5bebc337630111c02d3a31517114963080202327
jamesblakeymilner <jamesblakeymilner@gmail.com>
2018-03-06 10:27:52 +0200

    Update JTAG disable example
    Added comment referring to an issue raised. Added blink code to test outputs

--- Examples/JTAG_Software_Disable/JTAG_Software_Disable.ino
+++ Examples/JTAG_Software_Disable/JTAG_Software_Disable.ino
@@ -1,10 +1,30 @@
 void setup() {
   // Disable JTAG
+  // Note there is an open issue regarding this code.
+  // Refer to the link below for further details.
+  // So far it works in all tests. 
+  // https://github.com/Atlantis-Specialist-Technologies/CAN485/issues/2
+  
   MCUCR = (1<<JTD);
   MCUCR = (1<<JTD);
+
+  // Set JTAG pins as outputs
+  pinMode(A4, OUTPUT);
+  pinMode(A5, OUTPUT);
+  pinMode(A6, OUTPUT);
+  pinMode(A7, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // Blink the JTAG pins to test them
+  digitalWrite(A4, HIGH);
+  digitalWrite(A5, HIGH);
+  digitalWrite(A6, HIGH);
+  digitalWrite(A7, HIGH);
+  delay(1000);
+  digitalWrite(A4, LOW);
+  digitalWrite(A5, LOW);
+  digitalWrite(A6, LOW);
+  digitalWrite(A7, LOW);
+  delay(1000);
 }
